8182f728f40486ccc9997c84ebe5f9cc95aa18fe
apple <tutu3712@gmail.com>
2018-05-06 02:00:20 +0800

    init commited

---
+++ server.js
@@ -0,0 +1,56 @@
+const express = require("express")
+const hbs = require("hbs")
+const fs = require("fs")
+
+//server app
+var app = express()
+//middleware 
+hbs.registerPartials(__dirname + "/views/partials")
+app.use(express.static(__dirname + "/public"))
+app.set("view engine", hbs)
+app.use((req, res, next) => {
+    const now = new Date().toString()
+    const log = `${now}: ${req.method} ${req.url}`
+    console.log(log)
+    fs.appendFile("server.log", log + "\n", (err) => {
+        if(err){
+            console.log("Unable to append the file")
+        }
+    })
+    next()
+})
+app.use((req, res, next) => {
+    res.render("maintainance.hbs")
+    next()
+})
+
+//handlebars middleware
+
+hbs.registerHelper("getCurrentYear", () => {
+    return new Date().getFullYear()
+})
+hbs.registerHelper("screamIt", (text) => {
+    return text.toUpperCase()
+})
+
+
+//route
+app.get("/", (req, res) => {
+    res.render("index.hbs", {
+        title: "Home Page",
+
+    })
+})
+
+app.get("/about", (req, res) => {
+    res.render("about.hbs", {
+        title: "About Page"
+    })
+})
+
+
+
+//server listen
+app.listen(3000, () => {
+    console.log("The app server is up on port 3000...")
+})
